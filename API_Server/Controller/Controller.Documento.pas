a01911495752e2ac664f347372b2997e837337ff
Pretetis <etionpretetis@gmail.com>
2026-03-17 10:05:00 -0300

    Primeiros testes com VPS da AWS

--- API_Server/Controller/Controller.Documento.pas
+++ API_Server/Controller/Controller.Documento.pas
@@ -3,7 +3,9 @@ unit Controller.Documento;
 interface
 
 uses
-  Horse, System.SysUtils, Model.Connection; // <-- Adicionamos a Model.Connection aqui!
+  Horse, System.SysUtils, Model.Connection, System.JSON,
+  FireDAC.Phys.MongoDBDataSet,  FireDAC.Comp.Client,
+  FireDAC.Phys.MongoDBWrapper;
 
 type
   TControllerDocumento = class
@@ -13,40 +15,71 @@ type
 
 implementation
 
-// --- Rota antiga de teste ---
 procedure GetDocumentos(Req: THorseRequest; Res: THorseResponse; Next: TProc);
 begin
-  Res.Send('Lista de documentos retornada com sucesso!');
+    Res.Send('Lista de documentos retornada com sucesso!');
 end;
 
-// --- NOVA Rota para testar o MongoDB ---
 procedure TestarBanco(Req: THorseRequest; Res: THorseResponse; Next: TProc);
 var
-  LConexao: TModelConnection;
+    LConexao: TModelConnection;
 begin
-  try
-    // Ao criar a classe, ela vai ler o config.ini e tentar conectar no Atlas
-    LConexao := TModelConnection.Create;
     try
-      if LConexao.Connection.Connected then
-        Res.Status(200).Send('Sucesso! A API conectou perfeitamente no MongoDB Atlas.')
-      else
-        Res.Status(500).Send('Falha: A classe foi criada, mas a conexão não está ativa.');
-    finally
-      LConexao.Free; // Libera a conexão da memória (muito importante em APIs!)
+        LConexao := TModelConnection.Create;
+        try
+            if LConexao.Connection.Connected then
+                Res.Status(200).Send('Sucesso! A API conectou perfeitamente no MongoDB.')
+            else
+                Res.Status(500).Send('Falha: A classe foi criada, mas a conexão não está ativa.');
+        finally
+            LConexao.Free; // Libera a conexão da memória (muito importante em APIs!)
+        end;
+    except
+        on E: Exception do
+            Res.Status(500).Send('Erro fatal ao conectar no banco: ' + E.Message);
     end;
-  except
-    on E: Exception do
-      // Se der erro de senha, IP bloqueado ou falta de DLL, o erro vai cair aqui
-      Res.Status(500).Send('Erro fatal ao conectar no banco: ' + E.Message);
-  end;
+end;
+
+procedure CadastrarDocumento(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+var
+    LJSONRecebido: TJSONObject;
+    LModelConexao: TModelConnection;
+    FMongoCon: TMongoConnection;
+    FMongoEnv: TMongoEnv;
+    FDatabase: TMongoDatabase;
+    FCollection: TMongoCollection;
+    LDocBSON: TMongoDocument;
+begin
+    LJSONRecebido := Req.Body<TJSONObject>;
+
+    LModelConexao := TModelConnection.Create;
+
+    try
+        LModelConexao.Connection.Connected := True;
+
+        FMongoCon := TMongoConnection(LModelConexao.Connection.CliObj);
+        FMongoEnv := FMongoCon.Env;
+        FDatabase := FMongoCon['AthenaDB'];
+        FCollection := FDatabase['Documentos'];
+
+        LDocBSON := FMongoEnv.NewDoc;
+        LDocBSON
+          .Add('funcionario', LJSONRecebido.GetValue('funcionario').Value)
+          .Add('tipo', LJSONRecebido.GetValue('tipo').Value)
+          .Add('validade', LJSONRecebido.GetValue('validade').Value);
+        FCollection.Insert(LDocBSON);
+
+        Res.Status(201).Send('Documento catalogado com sucesso!');
+    finally
+        LModelConexao.Free;
+    end;
 end;
 
 class procedure TControllerDocumento.Registry;
 begin
-  // Registra as rotas no Horse
-  THorse.Get('/documentos', GetDocumentos);
-  THorse.Get('/testar-banco', TestarBanco); // <-- Registramos a nova rota aqui
+    THorse.Get('/documentos', GetDocumentos);
+    THorse.Get('/testar-banco', TestarBanco);
+    THorse.Post('/cadastrar-documentos', CadastrarDocumento);
 end;
 
 end.
